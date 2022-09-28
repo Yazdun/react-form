f1135c950ae67d5f4e6791d5d87026455c21c76b
Yazdun <yazduntube@gmail.com>
2022-09-28 17:45:58 +0330

    commit

--- src/components/date/index.jsx
+++ src/components/date/index.jsx
@@ -1,5 +1,39 @@
 import css from './styles.module.css'
+import * as moment from 'jalali-moment'
+import { useState } from 'react'
+import cn from 'classnames'
 
 export const Date = () => {
-  return <div>تاریخ</div>
+  const [active, setActive] = useState(1)
+  const days = [1, 2, 3, 4, 5, 6]
+
+  const generator = num =>
+    moment().add(num, 'day').locale('fa').format('D/MMMM')
+
+  return (
+    <div>
+      <p className={css.title}>تاریخ</p>
+      <ul className={css.list}>
+        {days.map(i => {
+          const m = generator(i)
+          const day = m.split('/')[0]
+          const month = m.split('/')[1]
+          const isCurrent = active === i
+
+          return (
+            <li key={day}>
+              <button
+                onClick={() => setActive(i)}
+                className={cn(css.cta, isCurrent && css.active)}
+                disabled={isCurrent}
+              >
+                <span className={css.day}>{day}</span>
+                <span className={css.month}>{month}</span>
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
