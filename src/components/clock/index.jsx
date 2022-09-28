f1455868f3c6a7f5e13e34d4b0c0ea59b353a590
Yazdun <yazduntube@gmail.com>
2022-09-28 18:05:56 +0330

    commit

---
+++ src/components/clock/index.jsx
@@ -0,0 +1,5 @@
+import css from './styles.module.css'
+
+export const Clock = () => {
+  return <div>Clock</div>
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { InputError } from './inputError'
 export { Input } from './input'
 export { Date } from './date'
+export { Clock } from './clock'
