9bb0df073b794606c70562463f1f5e23b0a62f42
Yazdun <yazduntube@gmail.com>
2022-09-29 12:11:12 +0330

    commit

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -1,6 +1,4 @@
 import css from './styles.module.css'
-import Lottie from 'lottie-react'
-import animationContent from './scroll.json'
 
 export const Hero = () => {
   return (
@@ -9,15 +7,12 @@ export const Hero = () => {
         <h1 className={css.title}>
           مشاوره آنلاین <span>با حرفه ای ترین ها</span>
         </h1>
-        <p className={css.desc}>
+        {/* <p className={css.desc}>
           افتخار ما همراهی با ده‌ها سازمان، شتابدهنده، دانشگاه و مجموعه
           سرمایه‌گذاری در حوزه کسب و کار و کارآفرینی است. اگر برای رشد فردی یا
           کسب و کارت نیاز به تجربه و همفکری داری, مشاوره اختصاصی آنلاین رو
           امتحان کن
-        </p>
-        <a href="#form" className={css.lottie}>
-          <Lottie animationData={animationContent} width={400} height={400} />
-        </a>
+        </p> */}
       </div>
     </div>
   )
